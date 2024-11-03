62e145caba9aa2ea5f75e6c4002711a81b8a9f9e
Salman (laptop) <t123y2@hotmail.com>
2024-11-03 14:24:06 +0300

    added background color

--- app/(tabs)/bookmarks.tsx
+++ app/(tabs)/bookmarks.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { Colors } from '@/constants/Colors';
 
 export default function Bookmarks() {
   return (
-    <SafeAreaView>
-      <Text>Bookmarks</Text>
+    <SafeAreaView className='flex-1 items-center justify-center' style={{ backgroundColor: Colors.primary }}>
+      <View className=''>
+
+      </View>
     </SafeAreaView>
   );
 }
