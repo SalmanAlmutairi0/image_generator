c10faddefe2c1529c25562bdf98ddf2a5bcf7dca
salman almutairi <t123y2@gmail.com>
2024-11-05 19:58:36 +0300

    cleaning up some code

--- components/createModal.tsx
+++ components/createModal.tsx
@@ -10,7 +10,7 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import { Feather } from "@expo/vector-icons";
-import { Colors } from "@/constants/Colors";
+import { Colors, imageColors } from "@/constants/Colors";
 import FormGroup from "./formGroup";
 import { Images } from "@/constants/Images";
 
@@ -23,28 +23,28 @@ export default function CreateModal({ showModal, setShowModal }: Props) {
   const [selectedImage, setSelectedImage] = useState(-1);
   const [selectedcolor, setSelectedColor] = useState('');
 
-  const imageColors = [
-    { name: "Crimson", hex: "#DC143C" },
-    { name: "Deep Sky Blue", hex: "#00BFFF" },
-    { name: "Lime Green", hex: "#32CD32" },
-    { name: "Goldenrod", hex: "#DAA520" },
-    { name: "Medium Violet Red", hex: "#C71585" },
-    { name: "Coral", hex: "#FF7F50" },
-    { name: "Steel Blue", hex: "#4682B4" },
-    { name: "Tomato", hex: "#FF6347" },
-    { name: "Medium Sea Green", hex: "#3CB371" },
-    { name: "Orchid", hex: "#DA70D6" },
-    { name: "Slate Blue", hex: "#6A5ACD" },
-    { name: "Turquoise", hex: "#40E0D0" },
-    { name: "Misty Rose", hex: "#FFE4E1" },
-    { name: "Light Coral", hex: "#F08080" },
-    { name: "Papaya Whip", hex: "#FFEFD5" },
-    { name: "Peach Puff", hex: "#FFDAB9" },
-    { name: "Light Goldenrod Yellow", hex: "#FAFAD2" },
-    { name: "Lavender", hex: "#E6E6FA" },
-    { name: "Rosy Brown", hex: "#BC8F8F" },
-    { name: "Powder Blue", hex: "#B0E0E6" },
-  ];
+  // const imageColors = [
+  //   { name: "Crimson", hex: "#DC143C" },
+  //   { name: "Deep Sky Blue", hex: "#00BFFF" },
+  //   { name: "Lime Green", hex: "#32CD32" },
+  //   { name: "Goldenrod", hex: "#DAA520" },
+  //   { name: "Medium Violet Red", hex: "#C71585" },
+  //   { name: "Coral", hex: "#FF7F50" },
+  //   { name: "Steel Blue", hex: "#4682B4" },
+  //   { name: "Tomato", hex: "#FF6347" },
+  //   { name: "Medium Sea Green", hex: "#3CB371" },
+  //   { name: "Orchid", hex: "#DA70D6" },
+  //   { name: "Slate Blue", hex: "#6A5ACD" },
+  //   { name: "Turquoise", hex: "#40E0D0" },
+  //   { name: "Misty Rose", hex: "#FFE4E1" },
+  //   { name: "Light Coral", hex: "#F08080" },
+  //   { name: "Papaya Whip", hex: "#FFEFD5" },
+  //   { name: "Peach Puff", hex: "#FFDAB9" },
+  //   { name: "Light Goldenrod Yellow", hex: "#FAFAD2" },
+  //   { name: "Lavender", hex: "#E6E6FA" },
+  //   { name: "Rosy Brown", hex: "#BC8F8F" },
+  //   { name: "Powder Blue", hex: "#B0E0E6" },
+  // ];
 
   const handleSlectedImage = (index: number) => {
     setSelectedImage(index);
